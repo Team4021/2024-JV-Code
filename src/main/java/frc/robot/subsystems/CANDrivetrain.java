a947e499ff2d8d0384085e9ecf0047ef8c9f37d8
James <igknightion@gmail.com>
2024-02-05 15:58:07 -0600

    Swapped out motor controller

--- src/main/java/frc/robot/subsystems/CANDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CANDrivetrain.java
@@ -6,11 +6,12 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.DrivetrainConstants.*;
 
-import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 
 /* This class declares the subsystem for the robot drivetrain if controllers are connected via CAN. Make sure to go to
  * RobotContainer and uncomment the line declaring this subsystem and comment the line for PWMDrivetrain.
@@ -28,21 +29,25 @@ public class CANDrivetrain extends SubsystemBase {
    * member variables and perform any configuration or set up necessary on hardware.
    */
   public CANDrivetrain() {
-    //WPI_VictorSPX leftFront = new WPI_VictorSPX(kLeftFrontID);
-    CANSparkMax leftFront = new CANSparkMax(kLeftFrontID, MotorType.kBrushed);
-    CANSparkMax leftRear = new CANSparkMax(kLeftRearID, MotorType.kBrushed);
-    CANSparkMax rightFront = new CANSparkMax(kRightFrontID, MotorType.kBrushed);
-    CANSparkMax rightRear = new CANSparkMax(kRightRearID, MotorType.kBrushed);
-        /*Sets current limits for the drivetrain motors. This helps reduce the likelihood of wheel spin, reduces motor heating
-         *at stall (Drivetrain pushing against something) and helps maintain battery voltage under heavy demand */
-        leftFront.setSmartCurrentLimit(kCurrentLimit);
-        leftRear.setSmartCurrentLimit(kCurrentLimit);
-        rightFront.setSmartCurrentLimit(kCurrentLimit);
-        rightRear.setSmartCurrentLimit(kCurrentLimit);
+      WPI_VictorSPX leftFront = new WPI_VictorSPX(kLeftFrontID);
+      WPI_VictorSPX leftRear = new WPI_VictorSPX(kLeftRearID);
+      WPI_VictorSPX rightFront = new WPI_VictorSPX(kRightFrontID);
+      WPI_VictorSPX rightRear = new WPI_VictorSPX(kRightRearID);
+      //CANSparkMax leftFront = new CANSparkMax(kLeftFrontID, MotorType.kBrushed);
+      //CANSparkMax leftRear = new CANSparkMax(kLeftRearID, MotorType.kBrushed);
+      //CANSparkMax rightFront = new CANSparkMax(kRightFrontID, MotorType.kBrushed);
+      //CANSparkMax rightRear = new CANSparkMax(kRightRearID, MotorType.kBrushed);
+      
+      /*Sets current limits for the drivetrain motors. This helps reduce the likelihood of wheel spin, reduces motor heating
+        *at stall (Drivetrain pushing against something) and helps maintain battery voltage under heavy demand */
+      //leftFront.setSmartCurrentLimit(kCurrentLimit);
+      //leftRear.setSmartCurrentLimit(kCurrentLimit);
+      //rightFront.setSmartCurrentLimit(kCurrentLimit);
+      //rightRear.setSmartCurrentLimit(kCurrentLimit);
 
-        // Set the rear motors to follow the front motors.
-        leftRear.follow(leftFront);
-        rightRear.follow(rightFront);
+      // Set the rear motors to follow the front motors.
+      leftRear.follow(leftFront);
+      rightRear.follow(rightFront);
       
 
       // Invert the left side so both side drive forward with positive motor outputs
